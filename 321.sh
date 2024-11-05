750dda357948ad677b717d71c2bfa1e3bd334c03
elibp <imelibp2010@gmail.com>
2024-11-04 20:31:13 -0900

    Create 321.sh

---
+++ 321.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+main() {
+    clear
+    echo -e "Downloading Latest Roblox..."
+    [ -f ./RobloxPlayer.zip ] && rm ./RobloxPlayer.zip
+    local robloxVersionInfo=$(curl -s "https://clientsettingscdn.roblox.com/v2/client-version/MacPlayer")
+    local versionInfo=$(curl -s "https://git.raptor.fun/main/version.json")
+    
+    local mChannel=$(echo $versionInfo | ./jq -r ".channel")
+    local version=$(echo $versionInfo | ./jq -r ".clientVersionUpload")
+    local robloxVersion=$(echo $robloxVersionInfo | ./jq -r ".clientVersionUpload")
+    
+    if [ "$version" != "$robloxVersion" ] && [ "$mChannel" == "preview" ]
+    then
+        curl "http://setup.rbxcdn.com/mac/$robloxVersion-RobloxPlayer.zip" -o "./RobloxPlayer.zip"
+    else
+        curl "http://setup.rbxcdn.com/mac/$version-RobloxPlayer.zip" -o "./RobloxPlayer.zip"
+    fi
+    
+    rm ./jq
+    echo -n "Installing Latest Roblox... "
+    [ -d "/Applications/Roblox.app" ] && rm -rf "/Applications/Roblox.app"
+    unzip -o -q "./RobloxPlayer.zip"
+    mv ./RobloxPlayer.app /Applications/Roblox.app
+    rm ./RobloxPlayer.zip
+    echo -e "Done."
